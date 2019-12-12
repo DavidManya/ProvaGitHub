259e392468f6ec0d6727778aaf273f8c0e8cb512
David Manyà <david.manya@gmail.com>
2019-12-12 12:30:51 +0100

    Acabat prEstructurasCondicionales2

---
+++ prEstructurasCondicionales2/prEstructurasCondicionales2/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace prEstructurasCondicionales2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtladoa = new System.Windows.Forms.TextBox();
+            this.txtladob = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnnuevo = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtarea = new System.Windows.Forms.TextBox();
+            this.txtperimetro = new System.Windows.Forms.TextBox();
+            this.pxcuadrado = new System.Windows.Forms.PictureBox();
+            this.pxrectangulo = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pxcuadrado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pxrectangulo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnsalir);
+            this.groupBox1.Controls.Add(this.btnnuevo);
+            this.groupBox1.Controls.Add(this.btncalcular);
+            this.groupBox1.Controls.Add(this.txtladob);
+            this.groupBox1.Controls.Add(this.txtladoa);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(424, 162);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingrese lados del Cuadrilátero";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtperimetro);
+            this.groupBox2.Controls.Add(this.txtarea);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(200, 201);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(245, 131);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lado A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lado B:";
+            // 
+            // txtladoa
+            // 
+            this.txtladoa.Location = new System.Drawing.Point(23, 50);
+            this.txtladoa.Name = "txtladoa";
+            this.txtladoa.Size = new System.Drawing.Size(105, 20);
+            this.txtladoa.TabIndex = 2;
+            // 
+            // txtladob
+            // 
+            this.txtladob.Location = new System.Drawing.Point(23, 110);
+            this.txtladob.Name = "txtladob";
+            this.txtladob.Size = new System.Drawing.Size(105, 20);
+            this.txtladob.TabIndex = 3;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(305, 30);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(88, 20);
+            this.btncalcular.TabIndex = 4;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnnuevo
+            // 
+            this.btnnuevo.Location = new System.Drawing.Point(305, 70);
+            this.btnnuevo.Name = "btnnuevo";
+            this.btnnuevo.Size = new System.Drawing.Size(88, 20);
+            this.btnnuevo.TabIndex = 5;
+            this.btnnuevo.Text = "Nuevo";
+            this.btnnuevo.UseVisualStyleBackColor = true;
+            this.btnnuevo.Click += new System.EventHandler(this.btnnuevo_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(305, 110);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(88, 20);
+            this.btnsalir.TabIndex = 6;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Área:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Perímetro:";
+            // 
+            // txtarea
+            // 
+            this.txtarea.Location = new System.Drawing.Point(104, 22);
+            this.txtarea.Name = "txtarea";
+            this.txtarea.Size = new System.Drawing.Size(93, 20);
+            this.txtarea.TabIndex = 2;
+            // 
+            // txtperimetro
+            // 
+            this.txtperimetro.Location = new System.Drawing.Point(104, 78);
+            this.txtperimetro.Name = "txtperimetro";
+            this.txtperimetro.Size = new System.Drawing.Size(93, 20);
+            this.txtperimetro.TabIndex = 3;
+            // 
+            // pxcuadrado
+            // 
+            this.pxcuadrado.BackColor = System.Drawing.Color.Blue;
+            this.pxcuadrado.Location = new System.Drawing.Point(44, 218);
+            this.pxcuadrado.Name = "pxcuadrado";
+            this.pxcuadrado.Size = new System.Drawing.Size(101, 101);
+            this.pxcuadrado.TabIndex = 2;
+            this.pxcuadrado.TabStop = false;
+            // 
+            // pxrectangulo
+            // 
+            this.pxrectangulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pxrectangulo.Location = new System.Drawing.Point(20, 234);
+            this.pxrectangulo.Name = "pxrectangulo";
+            this.pxrectangulo.Size = new System.Drawing.Size(148, 68);
+            this.pxrectangulo.TabIndex = 3;
+            this.pxrectangulo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(471, 450);
+            this.Controls.Add(this.pxrectangulo);
+            this.Controls.Add(this.pxcuadrado);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = ".:. Figuras Geométricas .:.";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pxcuadrado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pxrectangulo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.Button btnnuevo;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.TextBox txtladob;
+        private System.Windows.Forms.TextBox txtladoa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtperimetro;
+        private System.Windows.Forms.TextBox txtarea;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pxcuadrado;
+        private System.Windows.Forms.PictureBox pxrectangulo;
+    }
+}
+
---
+++ prEstructurasCondicionales2/prEstructurasCondicionales2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prEstructurasCondicionales2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pxcuadrado.Visible = false;
+            pxrectangulo.Visible = false;
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            //Declaración de variables
+            double ladoa, ladob;
+            double area, perimetro;
+            //Entrada de datos
+            ladoa = Convert.ToDouble(txtladoa.Text);
+            ladob = Convert.ToDouble(txtladob.Text);
+            //Proceso
+            //Evaluamos si es un cuadrado o un rectángulo
+            if (ladoa == ladob) 
+            {
+                pxcuadrado.Visible = true;
+                pxrectangulo.Visible = false;
+            }
+            else 
+            {
+                pxcuadrado.Visible = false;
+                pxrectangulo.Visible = true;
+            }
+            //Calcular área y perímetro
+            area = ladoa * ladob;
+            perimetro = 2 * (ladoa + ladob);
+            //Salida de la información
+            txtarea.Text = Convert.ToString(area);
+            txtperimetro.Text = Convert.ToString(perimetro);
+
+        }
+
+        private void btnnuevo_Click(object sender, EventArgs e)
+        {
+            txtladoa.Clear();
+            txtladob.Clear();
+            txtarea.Clear();
+            txtperimetro.Clear();
+            pxcuadrado.Visible = false;
+            pxrectangulo.Visible = false;
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
