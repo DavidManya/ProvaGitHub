8be90be1fb1a1854c637c2ec8a00666e78d49a1b
David Manyà <david.manya@gmail.com>
2019-12-13 12:54:16 +0100

    Acabat prControlTimer

---
+++ prControlTimer/prControlTimer/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace prControlTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblhora = new System.Windows.Forms.Label();
+            this.btnplay = new System.Windows.Forms.Button();
+            this.btnstop = new System.Windows.Forms.Button();
+            this.lbla = new System.Windows.Forms.Label();
+            this.lblb = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblhora
+            // 
+            this.lblhora.AutoSize = true;
+            this.lblhora.Font = new System.Drawing.Font("Corporation Games Straight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblhora.ForeColor = System.Drawing.Color.Gray;
+            this.lblhora.Location = new System.Drawing.Point(290, 37);
+            this.lblhora.Name = "lblhora";
+            this.lblhora.Size = new System.Drawing.Size(94, 29);
+            this.lblhora.TabIndex = 0;
+            this.lblhora.Text = "label1";
+            // 
+            // btnplay
+            // 
+            this.btnplay.Location = new System.Drawing.Point(311, 87);
+            this.btnplay.Name = "btnplay";
+            this.btnplay.Size = new System.Drawing.Size(63, 22);
+            this.btnplay.TabIndex = 1;
+            this.btnplay.Text = "Play";
+            this.btnplay.UseVisualStyleBackColor = true;
+            this.btnplay.Click += new System.EventHandler(this.btnplay_Click);
+            // 
+            // btnstop
+            // 
+            this.btnstop.Location = new System.Drawing.Point(311, 140);
+            this.btnstop.Name = "btnstop";
+            this.btnstop.Size = new System.Drawing.Size(63, 22);
+            this.btnstop.TabIndex = 2;
+            this.btnstop.Text = "Stop";
+            this.btnstop.UseVisualStyleBackColor = true;
+            this.btnstop.Click += new System.EventHandler(this.btnstop_Click);
+            // 
+            // lbla
+            // 
+            this.lbla.AutoSize = true;
+            this.lbla.BackColor = System.Drawing.Color.White;
+            this.lbla.Font = new System.Drawing.Font("Corporation Games Straight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbla.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lbla.Location = new System.Drawing.Point(107, 113);
+            this.lbla.Name = "lbla";
+            this.lbla.Size = new System.Drawing.Size(140, 29);
+            this.lbla.TabIndex = 3;
+            this.lbla.Text = "TSUNAMI";
+            // 
+            // lblb
+            // 
+            this.lblb.AutoSize = true;
+            this.lblb.BackColor = System.Drawing.Color.White;
+            this.lblb.Font = new System.Drawing.Font("Corporation Games Straight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblb.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.lblb.Location = new System.Drawing.Point(107, 113);
+            this.lblb.Name = "lblb";
+            this.lblb.Size = new System.Drawing.Size(140, 29);
+            this.lblb.TabIndex = 4;
+            this.lblb.Text = "TSUNAMI";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(505, 450);
+            this.Controls.Add(this.lblb);
+            this.Controls.Add(this.lbla);
+            this.Controls.Add(this.btnstop);
+            this.Controls.Add(this.btnplay);
+            this.Controls.Add(this.lblhora);
+            this.Name = "Form1";
+            this.Text = ".:. Control Timer .:.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblhora;
+        private System.Windows.Forms.Button btnplay;
+        private System.Windows.Forms.Button btnstop;
+        private System.Windows.Forms.Label lbla;
+        private System.Windows.Forms.Label lblb;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
